053ee0ad5e62426157b5dd564a0e15d39e799985
yoha-ni <yohan7913@naver.com>
2025-03-23 18:47:17 +0900

    test

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
-public class Main {
-    public static void main(String[] args) {
-        //TIP 캐럿을 강조 표시된 텍스트에 놓고 <shortcut actionId="ShowIntentionActions"/>을(를) 누르면
-        // IntelliJ IDEA이(가) 수정을 제안하는 것을 확인할 수 있습니다.
-        System.out.printf("Hello and welcome!");
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/>을(를) 눌러 코드 디버그를 시작하세요. 1개의 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 중단점을 설정해 드렸습니다
-            // 언제든 <shortcut actionId="ToggleLineBreakpoint"/>을(를) 눌러 중단점을 더 추가할 수 있습니다.
-            System.out.println("i = " + i);
+public class Main extends Thread {
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executor = Executors.newFixedThreadPool(5);
+
+        for (int i = 1; i <= 100; i++) {
+            final int taskId = i;
+            executor.execute(()->{
+                System.out.println("😎 작업중 " + taskId + " 실행 (스레드: " + Thread.currentThread().getName() + ")");
+                try {Thread.sleep(1000);} catch (InterruptedException e) {}
+                System.out.println("👌 작업 " + taskId + " 완료");
+            });
         }
+        executor.shutdown();
     }
 }
